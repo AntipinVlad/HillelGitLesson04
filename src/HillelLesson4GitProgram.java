4d9e352d52c90fc36f1065d17795531686fa310f
AntipinVlad <antipinmail1@gmail.com>
2024-01-09 20:42:11 +0200

    add new system out println

--- src/HillelLesson4GitProgram.java
+++ src/HillelLesson4GitProgram.java
@@ -1,5 +1,6 @@
 public class HillelLesson4GitProgram {
     public static void main(String[] args) {
         System.out.println("Hello world from Vlad to Git");
+        System.out.println("Second commit to Git");
     }
 }
